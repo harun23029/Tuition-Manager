f6d85686030fbf0424fe056d85bd61d306ce570f
harun23029 <harunducse23rd@gmail.com>
2018-11-25 02:55:51 +0600

    Updated Home Page

--- app/src/main/java/com/example/coder87/tuitionmanager/HomePage.kt
+++ app/src/main/java/com/example/coder87/tuitionmanager/HomePage.kt
@@ -7,22 +7,43 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ScrollView
 import kotlinx.android.synthetic.main.activity_home_page2.*
 
 class HomePage : AppCompatActivity() {
+    private lateinit var homePage: ScrollView
+    private lateinit var profilePage: ScrollView
+    private lateinit var notificationsPage: ScrollView
+
+    private fun bindWidgets()
+    {
+        homePage=findViewById(R.id.home_page)
+        profilePage=findViewById(R.id.profile_page)
+        notificationsPage=findViewById(R.id.notifications_page)
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                bindWidgets()
+                profilePage.visibility= View.INVISIBLE
+                notificationsPage.visibility= View.INVISIBLE
+                homePage.visibility= View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
-                message.setText(R.string.title_profile)
+                notificationsPage.visibility=View.INVISIBLE
+                homePage.visibility=View.INVISIBLE
+                profilePage.visibility=View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                bindWidgets()
+                homePage.visibility=View.INVISIBLE
+                profilePage.visibility=View.INVISIBLE
+                notificationsPage.visibility=View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
         }
